9b5efda5ce07ea2465f6f8be7b88b77893411ea3
csw001 <eudemonia_c@foxmail.com>
2019-08-30 18:44:48 +0800

    'pdf和png可以随时互换？'

--- app.js
+++ app.js
@@ -20,22 +20,33 @@ app.use((req, res, next) => {
     next()
 })
 app.use('/public', express.static(staticDir));
+app.use('/phantom', function (req, res) {
+    const phantom = require('phantom');
 
-const phantom = require('phantom');
+    (async function () {
+        const instance = await phantom.create();
+        const page = await instance.createPage();
+        await page.property('viewportSize');
+        await page.on('onResourceRequested', function (requestData) {
+            console.info('Requesting', requestData.url);
+        });
 
-(async function () {
-    const instance = await phantom.create();
-    const page = await instance.createPage();
-    await page.on('onResourceRequested', function (requestData) {
-        console.info('Requesting', requestData.url);
-    });
+        await page.open('https://www.baidu.com');
+        // const content = await page.property('content');
 
-    const status = await page.open('https://baidu.com/');
-    const content = await page.property('content');
-    console.log(content);
+        // console.log(`Page opened with status [${status}].`);
+        // console.log(content);
+        await page.evaluateJavaScript(`function() { 
+            document.body.innerHTML = '<div>1231231312316sfdg sfgsf1313<div>'
+            return div
+         }`)
+        await page.render('stackoverflow.png');
 
-    await instance.exit();
-})();
+        await instance.exit();
+
+        res.end('成功咯')
+    })();
+})
 
 if (!module.parent) {
     app.listen(3003, function () {
